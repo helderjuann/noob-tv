e44d45c2615c676a823917f7ed2099d23cc0b83c
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-09-27 17:32:41 -0300

    Add files via upload

--- js/movie.js
+++ js/movie.js
@@ -1,117 +1,61 @@
-var main = document.querySelectorAll('.main .center')[0];
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1')
-.then(response=> response.json())
-.then(data=>{
-        
-        data.results.map((i,j)=>{
-           
-            if(j > 15)
-                return;
-            if(j == 0){
-                main.innerHTML+=`
-                
-                    <div class="banner-principal" style="background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-                
-                
-                `;
-            }else{
-                if(j == 1){
-                    main.innerHTML+=`
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
-                    <h2>Assista o melhor filme</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
-                    
-                    
-                    `;
-                }else{
-                    main.innerHTML+=`
+getMovies(API_URL)
 
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
-                    
-                    
-                    `;
-                }
-            }
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    showMovies(data.results)
+}
 
-        })
+function showMovies(movies) {
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const searchTerm = search.value
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
 })
 
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=2')
-.then(response=> response.json())
-.then(data=>{
-        
-        data.results.map((i,j)=>{
-           
-            if(j > 15)
-                return;
-            if(j == 0){
-                main.innerHTML+=`
-                
-                    <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-                
-                
-                `;
-            }else{
-                if(j == 1){
-                    main.innerHTML+=`
-
-                    <h2>Assista o melhor filme</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
-                    
-                    
-                    `;
-                }else{
-                    main.innerHTML+=`
-
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
-                    
-                    
-                    `;
-                }
-            }
-
-        })
-})
-
-
-
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=3')
-.then(response=> response.json())
-.then(data=>{
-        
-        data.results.map((i,j)=>{
-           
-            if(j > 15)
-                return;
-            if(j == 0){
-                main.innerHTML+=`
-                
-                    <div class="banner-principal" style="margin-top:20px;background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-                
-                
-                `;
-            }else{
-                if(j == 1){
-                    main.innerHTML+=`
-
-                    <h2>Assista o melhor filme</h2>
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
-                    
-                    
-                    `;
-                }else{
-                    main.innerHTML+=`
-
-                    
-                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
-                    
-                    
-                    `;
-                }
-            }
-
-        })
-})
+function refreshPage(){
+    window.location.reload();
+}
